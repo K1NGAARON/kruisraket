25c2e28acb1faeafe27dec96889d4e43ab5896c2
K1NGAARON <gerardaaron1997@gmail.com>
2023-11-01 13:08:51 +0100

    changes

---
+++ test.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll('.p-ripple.p-element.p-button.p-component');
+// const lastButton = buttons[buttons.length - 1];
+
+console.log(buttons);
+// console.log(lastButton);
